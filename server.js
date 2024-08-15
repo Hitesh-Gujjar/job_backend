6b73974c361da1b3a0c36e32365f27c2dcd3b657
Hitesh-Gujjar <hiteshg.gujjar@gmail.com>
2024-08-15 13:10:08 +0530

    config vercel

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-// import app from './src/app/app.js';
-// import dotenv from 'dotenv'
-// import mongoConnect from './mongodb/mongodb.js'
-// dotenv.config()
+import app from './src/app/app.js';
+import dotenv from 'dotenv'
+import mongoConnect from './mongodb/mongodb.js'
+dotenv.config()
 
-import express from "express";
-const app = express();
-const port = 5000;
-// mongoConnect();
+const port = 8000;
+mongoConnect();
 
 app.listen(port,
     () => console.log("port is runing on", port)
