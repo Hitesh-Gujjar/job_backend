2fce756553f47abab1daa6af4c3d1f876ee42a5c
Hitesh-Gujjar <hiteshg.gujjar@gmail.com>
2024-08-15 13:29:08 +0530

    get job list api without auth

--- src/route/route.js
+++ src/route/route.js
@@ -4,9 +4,7 @@ import  jobRoutes from '../route/jobs-route/jobs.js'
 const router = express.Router();
 
 //admin routes;
-router.use('/', (req,res)=>{
-return res.status(200).json({"messa":"gdfdgasdfg"})
-});
+
 router.use('/job-portal/admin/user/job', jobRoutes);
 router.use('/job-portal/admin/user', adminUsersRoutes);
 
